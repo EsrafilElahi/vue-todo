34b2b605f159d2ca501405f3b9a10e660cea3f58
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-07 16:45:16 +0330

    install vue router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-red-900 underline">
+  <h1 class="text-3xl font-bold text-secondary-100 underline">
+    Hello world!
+  </h1>
+  <h1 class="text-3xl font-bold text-secondary-200 underline">
+    Hello world!
+  </h1>
+
+  <h1 class="text-3xl font-bold text-secondary-300 underline">
     Hello world!
   </h1>
 </template>
