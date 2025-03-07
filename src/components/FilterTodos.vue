e620daefded722a660460c1b1ad5570aeed25915
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-08 00:01:20 +0330

    add filterTodos component

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const emit = defineEmits(["handleSelectFilter"]);
+</script>
+
+<template>
+  <div class="flex-center gap-3">
+    <h2>Filters :</h2>
+    <div class="flex-center gap-3">
+      <button class="btn" @click="emit('handleSelectFilter', 'all')">
+        All
+      </button>
+      <button class="btn" @click="emit('handleSelectFilter', 'low')">
+        Low
+      </button>
+      <button class="btn" @click="emit('handleSelectFilter', 'high')">
+        High
+      </button>
+      <button class="btn" @click="emit('handleSelectFilter', 'done')">
+        Done
+      </button>
+    </div>
+  </div>
+</template>
