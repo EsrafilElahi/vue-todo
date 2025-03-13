4dd8aceb96acf9e4322c92e69d4cac7f83710395
Esrafil Elahi <Elahi.Esrafil@okala.com>
2025-03-13 21:45:01 +0330

    add type

--- src/types/allTypes.ts
+++ src/types/allTypes.ts
@@ -1,12 +1,20 @@
+export type Priority = "low" | "mid" | "high"
+
 export interface Todo {
   id: number;
   title: string;
-  priority: "low" | "mid" | "high";
+  priority: Priority;
   description: string;
   done: boolean;
 }
 
-export type FilterNames = 'all' | 'low' | 'high' | 'done'
+export type FilterNames = 'all' | Priority
+
+export interface PriorityOption {
+  id: number;
+  label: Capitalize<Priority>;
+  value: Priority
+}
 
 export interface TypeEmit {
   (e: "update:modelValue", value: string): void;
